719f31602d257f8a2071fe3fe09e725f6efa7a7c
rickfurt <ricardofurtado2609@gmail.com>
2018-12-18 23:04:28 +1100

    second

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'About me...'
+ });
+});
+
+router.get('/test', function(req, res, next) {
+  res.render('test', {
+    title: 'Test PAge..'
+ });
 });
 
 module.exports = router;
