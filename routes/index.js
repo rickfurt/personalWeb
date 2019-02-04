fd00482dbd91295037f3227d777cc798258fcbf1
rickfurt <ricardofurtado2609@gmail.com>
2019-02-04 14:35:51 +1100

    Fixed metadata and page title

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Coming soon...'
- });
-});
-
-router.get('/test', function(req, res, next) {
-  res.render('test', {
-    title: 'Test PAge..'
+    title: 'Ricardo Furtado - Resume '
  });
 });
 
